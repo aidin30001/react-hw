20692b27a38d24e4b5a79f0c5ddb1e0801b4bc80
айдин шаршекеев <toffiaidin01052004@gmail.com>
2025-02-12 17:38:55 +0600

    fixed backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 
-const data = [
-  user = {
+const data = {
+  user: {
     name: "Tom",
     age: 27,
     email: "tom@gmail.com"
   },
-  photo = "https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-male-user-profile-vector-illustration-isolated-background-man-profile-sign-business-concept_157943-38764.jpg?semt=ais_hybrid",
-  message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, eaque."
-]
+  photo: "https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-male-user-profile-vector-illustration-isolated-background-man-profile-sign-business-concept_157943-38764.jpg?semt=ais_hybrid",
+  message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, eaque."
+}
 
 function App() {
   return (
